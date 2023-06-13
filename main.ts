0d627a8ff83cd450609292e8c773b73b739449aa
moismat <mohamedismat91@gmail.com>
2023-06-13 17:07:52 +0200

    feat: hide cursor also on delay, add settings tab

--- main.ts
+++ main.ts
@@ -1,123 +1,103 @@
 import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-interface Settings {
-	reappearMode: "movement" | "delay";
-	threshold: number;
-	time: number;
+interface CursorSettings {
+	movementThreshold: number;
+	delayTime: number;
 }
 
-const DEFAULT_SETTINGS: Settings = {
-	reappearMode: "movement",
-	threshold: 3,
-	time: 500,
-}
+const DEFAULT_CURSOR_SETTINGS: CursorSettings = {
+	movementThreshold: 3,
+	delayTime: 500,
+};
 
-export default class PopoutWindowListenerPlugin extends Plugin {
-	settings: Settings;
-	scrollMoveHandler = this.scrollMoveWrapper.bind(this)
-	reappearMoveHandler = this.reappearMoveWrapper.bind(this)
-	scrollUnscrollHandler = this.scrollUnscrollWrapper.bind(this)
-	timeout: NodeJS.Timeout
-	scrolling = false
-	scroll = false
-	
+const HIDE_CURSOR_CLASS = "hide-cursor";
+
+export default class AutoHideCursorPlugin extends Plugin {
+	settings: CursorSettings;
+	cursorTimeout: NodeJS.Timeout;
+	isCursorMoving = false;
+
 	async onload() {
-		await this.loadSettings();
-		this.addSettingTab(new AHCSettings(this.app, this));
+		await this.loadUserSettings();
+		this.addSettingTab(new AutoHideCursorSettingsTab(this.app, this));
 
 		this.app.workspace.onLayoutReady(() => {
-			// movement else delay
-			if (this.settings.reappearMode === "movement") {
-				this.registerDomEvent(
-					document.body,
-					"scroll",
-					this.scrollMoveHandler,
-					{ capture: true }
-				);
+			this.setEventListeners(document.body);
+		});
 
-				this.registerDomEvent(
-					document.body,
-					"mousemove",
-					this.reappearMoveHandler,
-					{ capture: true }
-				);
-			} else {
-				this.registerDomEvent(
-					document.body,
-					"scroll",
-					this.scrollUnscrollHandler,
-					{ capture: true }
-				);
-			}
+		// to support popout windows
+		this.registerEvent(
+			this.app.workspace.on("layout-change", () => {
+				setTimeout(() => {
+					this.setEventListeners(activeDocument.body);
+				}, 1000);
+			})
+		);
+	}
+
+	setEventListeners(targetElement: HTMLElement) {
+		targetElement.removeEventListener(
+			"mousemove",
+			this.handleCursorMovement,
+			{ capture: true }
+		);
+		targetElement.removeEventListener("scroll", this.handleScrollEvent, {
+			capture: true,
+		});
+
+		this.registerDomEvent(
+			targetElement,
+			"mousemove",
+			this.handleCursorMovement,
+			{ capture: true }
+		);
+		this.registerDomEvent(targetElement, "scroll", this.handleScrollEvent, {
+			capture: true,
 		});
 	}
 
-	async loadSettings() {
-		this.settings = { ...DEFAULT_SETTINGS, ...await this.loadData() };
+	async loadUserSettings() {
+		this.settings = {
+			...DEFAULT_CURSOR_SETTINGS,
+			...(await this.loadData()),
+		};
 	}
 
-	async saveSettings() {
+	async saveUserSettings() {
 		await this.saveData(this.settings);
+		this.setEventListeners(activeDocument.body);
 	}
 
+	handleScrollEvent = () => {
+		document.body.classList.add(HIDE_CURSOR_CLASS);
+		this.isCursorMoving = false;
+	};
 
-	scrollMove = (targetElement: HTMLElement) => {
-		targetElement.classList.add("hide-cursor");
-		this.scroll = true
-	}
+	handleCursorMovement = (evt: MouseEvent) => {
+		if (this.hasCursorMovementExceededThreshold(evt)) {
+			clearTimeout(this.cursorTimeout);
+			document.body.classList.remove(HIDE_CURSOR_CLASS);
+			this.isCursorMoving = true;
 
-	scrollMoveWrapper() {
-		if (!this.scroll) {
-			this.scrollMove(document.body)
+			this.cursorTimeout = setTimeout(() => {
+				document.body.classList.add(HIDE_CURSOR_CLASS);
+				this.isCursorMoving = false;
+			}, this.settings.delayTime);
 		}
-	}
+	};
 
-	reappearMove = (evt: MouseEvent, targetElement: HTMLElement) => {
-		// to avoid listener always running 
-		if (this.scroll && this.isMouseMovementExceededThreshold(evt)) {
-			targetElement.classList.remove("hide-cursor");
-			this.scroll = false
-		}
-	}
-
-	reappearMoveWrapper(evt: MouseEvent) {
-		this.reappearMove(evt, document.body);
-	}
-
-	isMouseMovementExceededThreshold(evt: MouseEvent) {
-		const threshold = this.settings.threshold;
+	hasCursorMovementExceededThreshold(evt: MouseEvent) {
+		const movementThreshold = this.settings.movementThreshold;
 		const deltaX = Math.abs(evt.movementX);
 		const deltaY = Math.abs(evt.movementY);
-		return deltaX > threshold || deltaY > threshold;
-	}
-
-
-	scrollUnscroll = (targetElement: HTMLElement) => {
-		if (!this.scrolling) {
-			targetElement.classList.add("hide-cursor");
-			this.scrolling = true;
-		}
-
-		if (this.scrolling) {
-			clearTimeout(this.timeout);
-			this.timeout = setTimeout(() => {
-				targetElement.classList.remove("hide-cursor");
-				this.scrolling = false;
-				console.log("fini")
-			}, this.settings.time);
-		}
-	}
-
-	scrollUnscrollWrapper() {
-		this.scrollUnscroll(document.body)
+		return deltaX > movementThreshold || deltaY > movementThreshold;
 	}
 }
 
+class AutoHideCursorSettingsTab extends PluginSettingTab {
+	plugin: AutoHideCursorPlugin;
 
-class AHCSettings extends PluginSettingTab {
-	plugin: PopoutWindowListenerPlugin;
-
-	constructor(app: App, plugin: PopoutWindowListenerPlugin) {
+	constructor(app: App, plugin: AutoHideCursorPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -125,111 +105,33 @@ class AHCSettings extends PluginSettingTab {
 	display(): void {
 		const { containerEl } = this;
 		containerEl.empty();
-		containerEl.createEl('h2', { text: 'Auto Hide Cursor settings.' });
+		containerEl.createEl("h2", { text: "Auto Hide Cursor Settings" });
 
 		new Setting(containerEl)
-			.setName("Cursor Reappearance")
-			.setDesc("Reappear after a delay or after moving the cursor.")
-			.addDropdown((dropdown) => {
-				dropdown
-					.addOptions({
-						movement: "movement",
-						delay: "delay",
-					})
-					.setValue(this.plugin.settings.reappearMode)
-					.onChange(async (value: "movement" | "delay") => {
-						this.plugin.settings.reappearMode = value;
-						if (value === "movement") {
-							this.plugin.registerDomEvent(
-								document.body,
-								"scroll",
-								this.plugin.scrollMoveHandler,
-								{ capture: true }
-							);
-
-							this.plugin.registerDomEvent(
-								document.body,
-								"mousemove",
-								this.plugin.reappearMoveHandler,
-								{ capture: true }
-							);
-
-							document.body.removeEventListener(
-								'scroll',
-								this.plugin.scrollUnscrollHandler,
-								{ capture: true }
-							)
-
-						} else {
-							this.plugin.registerDomEvent(
-								document.body,
-								"scroll",
-								this.plugin.scrollUnscrollHandler,
-								{ capture: true }
-							);
-
-							document.body.removeEventListener(
-								'scroll',
-								this.plugin.scrollMoveHandler,
-								{ capture: true }
-							);
-							
-							document.body.removeEventListener(
-								'mousemove',
-								this.plugin.reappearMoveHandler,
-								{ capture: true }
-							);
-						}
-						this.plugin.saveSettings();
-					});
-			});
-		
-		new Setting(containerEl)
-			.setName("Move threshold in px. (default 3)")
-			.setDesc("Min. distance to show the cursor again in movement mode")
+			.setName("Movement Threshold (px)")
+			.setDesc("Minimum distance to show the cursor again")
 			.addSlider((slider) => {
 				slider
 					.setLimits(0, 10, 1)
-					.setValue(this.plugin.settings.threshold)
+					.setValue(this.plugin.settings.movementThreshold)
 					.setDynamicTooltip()
 					.onChange(async (value) => {
-						this.plugin.settings.threshold = value;
-						await this.plugin.saveSettings();
-					});
-			})
-			.addExtraButton(btn => {
-				btn
-					.setIcon("reset")
-					.setTooltip("Reset to default")
-					.onClick(async () => {
-						this.plugin.settings.threshold = DEFAULT_SETTINGS.threshold;
-						console.log("this.plugin.settings.moveThreshold", this.plugin.settings.threshold)
-						await this.plugin.saveSettings();
-						this.display()
+						this.plugin.settings.movementThreshold = value;
+						await this.plugin.saveUserSettings();
 					});
 			});
-		
+
 		new Setting(containerEl)
-			.setName("Time threshold in ms (default 500)")
-			.setDesc("Min. time to show the cursor again in delay mode")
+			.setName("Hide Delay (ms)")
+			.setDesc("Time to hide the cursor after stopping movement")
 			.addSlider((slider) => {
 				slider
-					.setLimits(200, 700, 25)
-					.setValue(this.plugin.settings.time)
+					.setLimits(200, 1000, 25)
+					.setValue(this.plugin.settings.delayTime)
 					.setDynamicTooltip()
 					.onChange(async (value) => {
-						this.plugin.settings.time = value;
-						await this.plugin.saveSettings();
-					});
-			})
-			.addExtraButton(btn => {
-				btn
-					.setIcon("reset")
-					.setTooltip("Reset to default")
-					.onClick(async () => {
-						this.plugin.settings.time = DEFAULT_SETTINGS.time;
-						await this.plugin.saveSettings();
-						this.display()
+						this.plugin.settings.delayTime = value;
+						await this.plugin.saveUserSettings();
 					});
 			});
 	}
